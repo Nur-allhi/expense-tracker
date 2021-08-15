375376163ccdc0a744576e94d767132f98825a19
Nur-allhi <noorefty1@gmail.com>
2021-08-15 21:20:36 +0600

    v-0.1.1

---
+++ app/changelog.md
@@ -0,0 +1,6 @@
+#### Expense Tracker,
+
+> Chnagelog: v-0.1.1
+
+- user image and name section added.
+- income and expenses status section added.
---
+++ app/screens/homescreen.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import { Image, StatusBar, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+function Homescreen(props) {
+    return (
+        <View style={styles.container}>
+            <StatusBar
+                backgroundColor="#FDFAF6"
+                barStyle="dark-content"
+            />
+            <View style={styles.userSectionWrapper}>
+                <Image style={styles.userImage} source={require('../assets/external/user.png')} />
+                <Text style={styles.userName}>
+                    Hi, Eftynur
+                </Text>
+            </View>
+            <View style={styles.currentStatusWrapper}>
+                <TouchableOpacity style={styles.balance}>
+                    <Text style={styles.balnceTitile}>Total Balance</Text>
+                    <Text style={styles.balanceAmount}>$1390.50</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.spendings}>
+                    <Text style={styles.spendingTitle}>Total spendings</Text>
+                    <Text style={styles.spendingAmount}>- $320.60</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: 10,
+        paddingHorizontal: 10,
+        backgroundColor: "#FDFAF6",
+    },
+    userSectionWrapper: {
+        paddingHorizontal: 10,
+        flexDirection: "row",
+        alignItems: "center"
+    },
+    userImage: {
+        width: 50,
+        height: 50,
+    },
+    userName: {
+        paddingHorizontal: 10,
+        fontSize: 18,
+        letterSpacing: 2,
+    },
+    currentStatusWrapper: {
+        paddingTop: 10,
+        flexDirection: "row",
+        justifyContent: "space-around"
+    },
+    balance: {
+        width: "45%",
+        height: 100,
+        borderRadius: 15,
+        backgroundColor: "#B5EAEA",
+        justifyContent: "center",
+        alignItems: "center"
+
+    },
+    balnceTitile: {
+        fontSize: 16,
+    },
+    balanceAmount: {
+        fontSize: 20,
+        fontWeight: "bold"
+
+    },
+    spendings: {
+        width: "45%",
+        height: 100,
+        borderRadius: 15,
+        backgroundColor: "#FE9898",
+        justifyContent: "center",
+        alignItems: "center"
+
+    },
+    spendingTitle: {
+        fontSize: 16,
+    },
+    spendingAmount: {
+        fontSize: 20,
+        fontWeight: "bold"
+    },
+})
+
+export default Homescreen;
